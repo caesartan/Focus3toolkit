55f1ac0d8ab63bea7d70098b3e745578e6df93f4
caesartan <caesartan@gmail.com>
2021-07-08 11:17:00 +0800

    file upload the first time

---
+++ GetFiles/MainWindow.xaml.vb
@@ -0,0 +1,20 @@
+﻿Class MainWindow
+    Private Sub Button1_Click(sender As Object, e As RoutedEventArgs) Handles Button1.Click
+        Dim p As New Process
+        p.StartInfo.FileName = "adb.exe"
+        p.StartInfo.Arguments = "pull /sdcard/Pictures/Screenshots/"
+        p.StartInfo.UseShellExecute = False
+        p.StartInfo.CreateNoWindow = True
+        p.Start()
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As RoutedEventArgs) Handles Button2.Click
+        Dim p As New Process
+        p.StartInfo.FileName = "adb.exe"
+        p.StartInfo.Arguments = "pull /sdcard/Movies/Screenrecorder/"
+        p.StartInfo.UseShellExecute = False
+        p.StartInfo.CreateNoWindow = True
+        p.Start()
+
+    End Sub
+
+End Class
